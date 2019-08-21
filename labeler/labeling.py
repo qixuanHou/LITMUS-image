94c666fa404f1d65fa3f1109c1ad8c5e91745640
EC2 Default User <ec2-user@ip-172-31-46-232.us-east-2.compute.internal>
2019-08-21 14:34:40 +0000

    update

--- labeler/labeling.py
+++ labeler/labeling.py
@@ -15,22 +15,33 @@ def getText():
     fp.close()
 
 
-
-sql = "select id, img from tweets where date like '%2018-01%' and img != 'None' and labels is null limit 500;"
-from google.cloud import vision
-client = vision.ImageAnnotatorClient()
-image = vision.types.Image()
-cursor.execute(sql)
-rows = cursor.fetchall()
-for r in rows:
-    image.source.image_uri = r[-1]
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-    labelList = []
-    for l in labels:
-        labelList.append(l.description)
-    sql = "update tweets set labels = '%s' where id = '%s'"%(str(labelList).replace("'","")[1:-1],r[0])
+def getLabels():
+    sql = "select id, img from tweets where date like '%2018-01%' and img != 'None' and labels is null limit 500;"
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
     cursor.execute(sql)
-    c.commit()
+    rows = cursor.fetchall()
+    for r in rows:
+        image.source.image_uri = r[-1]
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+        labelList = []
+        for l in labels:
+            labelList.append(l.description)
+        sql = "update tweets set labels = '%s' where id = '%s'"%(str(labelList).replace("'","")[1:-1],r[0])
+        cursor.execute(sql)
+        c.commit()
+
+def exportLabels():
+    sql = "select * from tweets where labels is not null;"
+    cursor.execute(sql);
+    rows = cursor.fetchall()
+    fp = open('2018-01-labels.csv', 'w')
+    myFile = csv.writer(fp)
+    myFile.writerows(rows)
+    fp.close()
+
+exportLabels()
 cursor.close()
 c.close()
